e7c273e7b46130be4a3422e70c9000fab1bca179
SYED FAWWAD <160050947+Syed-Fawwad@users.noreply.github.com>
2024-04-03 21:25:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber=Math.floor(Math.random()*10+1);
+
+const answer =await inquirer.prompt([
+    {message:"Please guess a number between 1 to 10.",
+    type:"number",
+    name:"userGuessNumber"},
+]);
+if(answer.userGuessNumber===randomNumber){
+    console.log("CONGRATULATIONS! you guess right number.")
+}else{
+    console.log("You guess a wrong number.")
+};
